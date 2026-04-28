73c123fd4721afa67d800abe5a4fa5b145552803
Riku Ueda <e245752@ie.u-ryukyu.ac.jp>
2026-04-28 09:54:40 +0900

    week3_3

--- week3/run.py
+++ week3/run.py
@@ -1,4 +1,8 @@
+import datasets
+import importlib
 import regression
+importlib.reload(regression)
+X,Y = datasets.load_linear_example1()
 model = regression.LinearRegression()
-print (model.x)
-#nothing
+print(model.fit(X,Y))
+print(model.theta)
